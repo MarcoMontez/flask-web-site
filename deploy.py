08e549d4c2bd6e2a49091fde9fd934d9a2824464
MarcoAntonioMontez <marco.montez.lis@gmail.com>
2020-04-10 12:01:22 +0100

    added templates to website

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,13 +6,13 @@ post = [
 
 	{
 		'author':'Marco Montez',
-		'title': 'BLog Post 1',
+		'title': 'Blog Post 1',
 		'content':'First post content',
 		'date_posted':'April 10,2020'													
 	},
 	{
 		'author':'Jane Doe',
-		'title': 'BLog Post 2',
+		'title': 'Blog Post 2',
 		'content':'Second post content',
 		'date_posted':'April 11,2020'														
 	}
@@ -22,8 +22,8 @@ post = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html',posts = post)
+    return render_template('home.html',posts = post,title='Home')
     
 @app.route('/about')
 def about():
-    return render_template('about.html') 
+    return render_template('about.html',posts = post,title='About') 
